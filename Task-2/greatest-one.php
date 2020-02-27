6379e3bb4da70b67f61b9ca451160311504f5e45
Binod Baral <cixwarebinod@gmail.com>
2020-02-27 08:39:34 +0545

    finding the greatest number among three of them

---
+++ Task-2/greatest-one.php
@@ -0,0 +1,16 @@
+<?php
+ $num1=36;
+ $num2=56;
+ $num3=25;
+ if($num1>$num2 && $num1>$num3){
+    echo "$num1 is the greatest one.";
+ }
+ else if($num2>$num1 && $num2>$num3)
+ {
+     echo "$num2 is the greatest one.";
+ }
+ else{
+    echo "$num3 is the greatest one.";
+ }
+
+  ?>
